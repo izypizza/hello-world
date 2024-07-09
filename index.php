a860236a0a220358352162ff181afc1b0e55a87d
IZYPIZZA <maxlobito30@gmail.com>
2024-07-09 16:44:21 -0500

    añadi un for para que se repita 10 veces el hola mundo

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
     <title>Hello World</title>
 </head>
 <body>
-    <h1>Hello World</h1>
+    <?php
+    for ($x = 0; $x <= 10; $x++){
+        echo "<h1>Hello World</h1>";
+    }
+    ?>
 </body>
 </html>
